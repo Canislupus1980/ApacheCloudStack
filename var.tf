d5ee68b9d52b34df69e3ee04f43e9d5ded7a170d
Siarhei Buhtiyarov <86954730+Canislupus1980@users.noreply.github.com>
2022-09-15 15:13:51 +0300

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "cloudstack_api_url" {
+    type = string
+    default = ""
+}
+
+variable "cloudstack_api_key" {
+    type = string
+    default = ""
+}
+
+variable "cloudstack_secret_key" {
+    type = string
+    default = ""
+}
+
+variable "zone" {
+    type = string
+    default = "bc-xcp-zone-01"
+}
+
+variable "name" {
+  type = string
+  default = "restapi"
+}
