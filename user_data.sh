99bd8f184753d874f4fc29560fd63abc6796b8ee
Siarhei Buhtiyarov <86954730+Canislupus1980@users.noreply.github.com>
2022-09-21 12:29:27 +0300

    Create user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt -y update && sudo apt -y upgrade
+#sudo dpkg --configure -a
+sudo apt -y install openjdk-11-jre && sudo apt -y install curl
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
