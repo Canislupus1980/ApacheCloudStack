ef36a94ab78b58343e521cdc40e63d691b6c35d2
Siarhei Buhtiyarov <86954730+Canislupus1980@users.noreply.github.com>
2022-09-21 12:26:47 +0300

    Update apache.tf

--- apache.tf
+++ apache.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     cloudstack = {
-      source = "cloudstack/cloudstack"
-      version = "0.4.0"
+      source            = "cloudstack/cloudstack"
+      version           = "0.4.0"
     }
   }
 }
 
 provider "cloudstack" {
-  api_url    = "${var.cloudstack_api_url}"
-  api_key    = "${var.cloudstack_api_key}"
-  secret_key = "${var.cloudstack_secret_key}"
+  api_url               = "${var.cloudstack_api_url}"
+  api_key               = "${var.cloudstack_api_key}"
+  secret_key            = "${var.cloudstack_secret_key}"
 }
 
 resource "cloudstack_instance" "Jenkins" {
@@ -22,10 +22,16 @@ resource "cloudstack_instance" "Jenkins" {
     service_offering    = "L-Small Instance"
     template            = "9420cb9e-3dbe-4e26-bf67-2ca70c0036d7"
     expunge             = true
+    user_data           = file("user_data.sh")
+
+    # optional parameter
+    keypair             = "jenkins"
+    root_disk_size      = 30
+
 
     tags = {
-        Name = "Jenkins Server"
-        Owner = "Sergey Buhtiyarov"
-        Project = "Terraform Cloudstack"
-    }
+        Name            = "Jenkins Server"
+        Owner           = "Sergey Buhtiyarov"
+        Project         = "Terraform Cloudstack CICD"
+    }    
 }
